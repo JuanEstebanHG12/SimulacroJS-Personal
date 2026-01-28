20cb38107ae37c93faa6ca32cefdf7935e1bec77
JuanEstebanHG12 <juanestebanhg12@gmail.com>
2026-01-28 16:44:29 -0500

    Login

---
+++ src/services/usersServices.js
@@ -0,0 +1,30 @@
+export async function getUsers() {
+    try {
+        const response = await fetch('http://localhost:3000/users')
+        const data = await response.json()
+        return data
+    } catch (error) {
+        console.error(error);
+    }
+
+}
+
+export async function login(email) {
+    const users = await getUsers()
+    try {
+        const userFind = users.filter(user => user.email == email)
+        //Delete password
+        /* const userCopy = { ...userFind }
+        delete userCopy.password */
+        if(userFind.length > 0){
+           sessionStorage.setItem('user', JSON.stringify(userFind))
+            return userFind
+        } else{
+            throw new Error("User doesn't exist")
+        }
+        
+    } catch (error) {
+        console.error(error);
+
+    }
+}
